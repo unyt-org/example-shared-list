20e91521de31e2fc15ba7549593a0325c8bd1e5a
Benedikt Strehle <benedikt.strehle@uni-ulm.de>
2023-10-17 12:45:12 +0200

    update context url param

--- frontend/entrypoint.tsx
+++ frontend/entrypoint.tsx
@@ -5,8 +5,7 @@ import "../common/theme.ts";
 import { Overview } from "common/components/Overview.tsx";
 
 export default {
-	'/:id': async (ctx) => {
-		const id = ctx.urlMatch.get("id")!; // get list id
+	'/:id': async (_, {id}) => {
 		return <List list={await Lists.get(id)}/> // render the list component
 	}
 } satisfies UIX.Entrypoint;
