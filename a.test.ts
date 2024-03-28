3322b84ec822f1cb3c328830048c3737b8c5eb61
Benedikt Strehle <benedikt.strehle@uni-ulm.de>
2024-03-28 15:15:05 +0100

    update test

--- a.test.ts
+++ a.test.ts
@@ -3,8 +3,10 @@ import { Test, Assert } from "unyt-tests/testing/test.ts";
 @Test export class NewAAAAbbTest {
 	
 	@Test([
-		1, 2
-	]) firstTest(v: number) {
+		1,2,3
+	]) 
+	
+	firstTest(v: number) {
 		Assert.equals(1, v)
 	}
 
